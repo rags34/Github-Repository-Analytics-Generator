fe69b2e5c312e781d35c9b74531ceaae85626ed2
Raghav Minhas <raghavminhas34@gmail.com>
2023-11-26 21:05:03 +0530

    Added README

---
+++ README.md
@@ -0,0 +1,28 @@
+# Github Repository Analytics Generator
+
+## Description
+
+Github Repository Analytics Generator is a Python script designed to fetch key information about GitHub repositories using the GitHub API. It retrieves details such as stars, forks, primary language, and description, providing users with essential analytics for any GitHub repository.
+
+## Instructions
+
+1. Run the Python script (`repository_analytics.py`) in your preferred Python environment.
+2. Follow the prompts to input the repository owner's name and repository name.
+3. View the fetched repository analytics.
+
+## Files
+
+- **repository_analytics.py**: Python script for generating GitHub repository analytics.
+- **README.md**: This file, providing instructions and information about the project.
+
+## Author
+
+[Raghav Minhas]
+
+## Date
+
+[26-11-2023]
+
+## Note
+
+This project serves educational purposes and can be customized to fit specific needs. Feel free to modify the script as required.
--- repository_analytics.py
+++ repository_analytics.py
@@ -1,26 +1,39 @@
 import requests
 
+# Function to fetch repository information using the GitHub API
 def fetch_repository_info(owner, repo_name, access_token):
+    # Prepare headers with the access token for authentication
     headers = {
         "Authorization": f"token {access_token}"
     }
+    
+    # Construct the URL to access the repository information
     url = f"https://api.github.com/repos/{owner}/{repo_name}"
     
+    # Send a GET request to the GitHub API
     response = requests.get(url, headers=headers)
     
+    # Check if the request was successful (status code 200)
     if response.status_code == 200:
+        # Return the JSON response containing repository information
         return response.json()
     else:
+        # Return None if the request failed
         return None
 
+# Main function to execute the code
 def main():
-    # Replace these values with your repository details and access token
+    # Ask the user to input repository details
     repository_owner = input("Enter the repository-owner's name: ")
     repository_name = input("Enter the repository-name: ")
+    
+    # Replace 'Enter Your API token after generating here' with your generated API token
     github_token = "Enter Your API token after generating here"
 
+    # Fetch repository information using the provided details and access token
     repo_info = fetch_repository_info(repository_owner, repository_name, github_token)
 
+    # Display repository information if fetched successfully
     if repo_info:
         print(f"Repository: {repo_info['full_name']}")
         print(f"Description: {repo_info['description']}")
@@ -28,7 +41,9 @@ def main():
         print(f"Forks: {repo_info['forks_count']}")
         print(f"Primary Language: {repo_info['language']}")
     else:
+        # Display a message if repository information couldn't be fetched
         print("Failed to fetch repository information.")
 
+# Execute the main function if this script is run directly
 if __name__ == "__main__":
     main()
